4f8034351ae586c3845e5565664a415e85a48e81
Adam Fowler <adamfowler71@gmail.com>
2024-06-30 09:24:51 +0100

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version:5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "swift-jobs-redis",
+    platforms: [.macOS(.v14), .iOS(.v17), .tvOS(.v17)],
+    products: [
+        .library(name: "JobsRedis", targets: ["JobsRedis"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/hummingbird-project/swift-jobs.git", branch: "main"),
+        .package(url: "https://github.com/swift-server/RediStack.git", from: "1.4.0"),
+    ],
+    targets: [
+        .target(name: "JobsRedis", dependencies: [
+            .product(name: "Jobs", package: "swift-jobs"),
+            .product(name: "RediStack", package: "RediStack"),
+        ]),
+        .testTarget(name: "JobsRedisTests", dependencies: [
+            .byName(name: "JobsRedis"),
+            .product(name: "Jobs", package: "swift-jobs"),
+        ]),
+    ]
+)
