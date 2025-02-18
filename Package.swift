c2c4744cf2b302fa90b4aabef070e1aa825d2d0b
Adam Fowler <adamfowler71@gmail.com>
2025-02-18 20:01:53 +0000

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:6.0
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "swift-jobs-redis",
-    platforms: [.macOS(.v15), .iOS(.v17), .tvOS(.v17)],
+    platforms: [.macOS(.v14), .iOS(.v17), .tvOS(.v17)],
     products: [
         .library(name: "JobsRedis", targets: ["JobsRedis"])
     ],
