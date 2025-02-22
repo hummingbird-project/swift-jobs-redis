e45bf31eb8a86b484e003213774f655e49762ae0
Adam Fowler <adamfowler71@gmail.com>
2025-02-22 15:56:05 +0000

    Don't revert RediStack version

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/hummingbird-project/swift-jobs.git", branch: "main"),
-        .package(url: "https://github.com/swift-server/RediStack.git", from: "1.4.0"),
+        .package(url: "https://github.com/swift-server/RediStack.git", from: "1.6.2"),
     ],
     targets: [
         .target(
